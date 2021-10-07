404ba8c997452383d4fa768756be192919e31587
martin <alvarez.martinjr@gmail.com>
2021-10-06 17:04:39 -0700

    redesigned navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,69 +1,20 @@
 import React, { useState } from 'react';
-import styled, { css } from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFilm } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import ToggleNav from './ToggleNav';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilm, faBars } from '@fortawesome/free-solid-svg-icons';
 
-const Nav = styled.nav`
+const Nav = styled.div`
+  padding: 0 1rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
   background-color: #082032;
-  padding: 0px 20px;
-  color: #718899;
-  position: relative;
-  ${({ menuIconClicked }) =>
-    menuIconClicked &&
-    css`
-      height: 150px;
-      align-items: flex-start;
-    `}
+  flex-wrap: wrap;
 `;
 
 const Logo = styled(FontAwesomeIcon)`
   color: #ff4c29;
-`;
-
-const H1 = styled.h1`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 15px;
-`;
-
-const Ul = styled.ul`
-  margin: 0;
-  padding: 0;
-  display: flex;
-  @media (max-width: 500px) {
-    position: absolute;
-    visibility: ${({ menuIconClicked }) =>
-      menuIconClicked ? `visible` : `hidden`};
-  }
-
-  ${({ menuIconClicked }) =>
-    menuIconClicked &&
-    css`
-      display: flex;
-      flex-direction: column;
-      top: 70px;
-      visibility: visible;
-      left: 80px;
-    `};
-`;
-
-const Li = styled.li`
-  list-style: none;
-`;
-
-const NavLink = styled(Link)`
-  text-decoration: none;
-  color: #718899;
-  padding: 1rem;
-  margin-top: ${({ menuIconClicked }) => (menuIconClicked ? `5px` : `0`)};
-  &:hover {
-    color: white;
-  }
 `;
 
 const BrandLink = styled(Link)`
@@ -71,54 +22,63 @@ const BrandLink = styled(Link)`
   color: #718899;
 `;
 
-const ToggleWrapper = styled.div`
-  @media (min-width: 500px) {
-    display: none;
-  }
-  ${({ menuIconClicked }) =>
-    menuIconClicked &&
-    css`
-      margin-top: 25px;
-    `}
+const H2 = styled.h2`
+  display: flex;
+  gap: 0.7rem;
 `;
 
-const Navbar = () => {
-  const [menuIconClicked, setMenuIconClicked] = useState(false);
+const Menu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  @media (max-width: 768px) {
+    overflow: hidden;
+    flex-direction: column;
+    width: 100%;
+    max-height: ${({ isOpen }) => (isOpen ? '300px' : '0')};
+    transition: max-height 0.3s ease-in;
+  }
+`;
 
-  const toggleNavigation = (e) => {
-    menuIconClicked ? setMenuIconClicked(false) : setMenuIconClicked(true);
-  };
+const NavLink = styled(Link)`
+  text-decoration: none;
+  color: #718899;
+  padding: 1rem 2rem;
+  text-align: center;
+  transition: all 0.3s ease-in;
+  font-size: 0.9rem;
+  &:hover {
+    color: #b4c8d6;
+  }
+`;
+
+const Bars = styled(FontAwesomeIcon)`
+  color: #718899;
+  cursor: pointer;
+  @media (min-width: 769px) {
+    display: none;
+  }
+`;
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <Nav menuIconClicked={menuIconClicked}>
+    <Nav>
       <BrandLink to="/">
-        <H1>
+        <H2>
           <Logo icon={faFilm} size="lg" />
-          <div>My Movie List</div>
-        </H1>
+          <span>My Movie List</span>
+        </H2>
       </BrandLink>
-      <div>
-        <Ul menuIconClicked={menuIconClicked}>
-          <Li>
-            <NavLink to="/" menuIconClicked={menuIconClicked}>
-              Home
-            </NavLink>
-          </Li>
-          <Li>
-            <NavLink to="/about" menuIconClicked={menuIconClicked}>
-              About
-            </NavLink>
-          </Li>
-        </Ul>
-      </div>
-      <ToggleWrapper
-        onClick={toggleNavigation}
-        menuIconClicked={menuIconClicked}
-      >
-        <ToggleNav toggleOn={menuIconClicked} />
-      </ToggleWrapper>
+      <Bars icon={faBars} size="lg" onClick={() => setIsOpen(!isOpen)} />
+      <Menu isOpen={isOpen}>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </Menu>
     </Nav>
   );
-};
+}
 
 export default Navbar;
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import MovieCard from '../layout/MovieCard';
 
 const H1 = styled.h1`
   color: #b4c8d6;
@@ -9,6 +10,7 @@ function Home() {
   return (
     <div>
       <H1>Home</H1>
+      <MovieCard />
     </div>
   );
 }
