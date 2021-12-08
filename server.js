e36fb590de2ede3eb58a22251df148ddca99edf6
martin <alvarez.martinjr@gmail.com>
2021-12-07 16:22:06 -0800

    prepare for deploy

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
 // Connect Database
 connectDB();
-
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to the Movie list API...' })
-);
 
 // Init middleware to send data (POST & PUT)
 app.use(express.json({ extended: false }));
@@ -17,6 +14,15 @@ app.use(express.json({ extended: false }));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/movies', require('./routes/movies'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
